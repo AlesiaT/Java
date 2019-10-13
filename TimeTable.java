c78dda50b1cb2545574955b6e6a4b2ef3ab60c42
AlesiaT <55447426+AlesiaT@users.noreply.github.com>
2019-10-13 14:41:55 +0100

    Add files via upload

---
+++ TimeTable.java
@@ -0,0 +1,21 @@
+public class TimeTable
+{
+    public static void main (String args[])
+    {
+        for(int i=1;i<=9;i++)
+        {
+            for(int j=1;j<=9;j++)
+            {
+                if (((i*j)/10) < 1)
+                {
+                    System.out.print(" " + (i * j) + " ");
+                }
+                else
+                {
+                    System.out.print(i * j + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
